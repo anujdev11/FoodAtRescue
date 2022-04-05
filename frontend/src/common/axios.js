845a0c17acf981f619f50b10b8190078f000d70e
Purvilkumar Sanjaysinh Bharthania <pr420329@dal.ca>
2022-04-05 00:26:36 -0300

    Update Link

--- frontend/src/common/axios.js
+++ frontend/src/common/axios.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const axios_api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://ec2-44-201-9-16.compute-1.amazonaws.com:8080/api",
 });
 
 export default axios_api;
